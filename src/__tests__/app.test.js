5e3d7e3afc1f72d41fef69210a0909f74d674620
Ryan Scharfer <ryan.scharfer@finanzcheck.de>
2020-10-30 14:59:32 +0100

    upgraded all packages to latest version, using mock service worker in test

--- src/__tests__/app.test.js
+++ src/__tests__/app.test.js
@@ -1,57 +1,55 @@
 import React from "react";
-import axios from "axios";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
 
-import { render } from "@testing-library/react";
+import { render, waitFor, screen } from "@testing-library/react";
 
 import App from "../components/App";
+
 const RAW_CURRENT_WEATHER_RESPONSE_SUCCESS = {
-  data: {
-    main: {
-      temp: 40,
-    },
-    weather: [
-      {
-        main: "Clouds",
-      },
-    ],
+  main: {
+    temp: 40,
   },
-  status: 200,
-  statusText: "OK",
+  weather: [
+    {
+      main: "Clouds",
+    },
+  ],
 };
 
 const RAW_FORECAST_WEATHER_SUCCESS = {
-  status: 200,
-  statusText: "OK",
-  data: {
-    list: [
-      { dt: 1604077200, temp: { day: 5.2 }, weather: [{ main: "Clouds" }] },
-      { dt: 1604163600, temp: { day: 5.2 }, weather: [{ main: "Clouds" }] },
-      { dt: 1604250000, temp: { day: 5.2 }, weather: [{ main: "Clouds" }] },
-      { dt: 1604336400, temp: { day: 5.2 }, weather: [{ main: "Clouds" }] },
-      { dt: 1604422800, temp: { day: 5.2 }, weather: [{ main: "Clouds" }] },
-      { dt: 1604509200, temp: { day: 5.2 }, weather: [{ main: "Clouds" }] },
-    ],
-  },
+  list: [
+    { dt: 1604077200, temp: { day: 5.2 }, weather: [{ main: "Clouds" }] },
+    { dt: 1604163600, temp: { day: 5.2 }, weather: [{ main: "Clouds" }] },
+    { dt: 1604250000, temp: { day: 5.2 }, weather: [{ main: "Clouds" }] },
+    { dt: 1604336400, temp: { day: 5.2 }, weather: [{ main: "Clouds" }] },
+    { dt: 1604422800, temp: { day: 5.2 }, weather: [{ main: "Clouds" }] },
+    { dt: 1604509200, temp: { day: 5.2 }, weather: [{ main: "Clouds" }] },
+  ],
 };
 
-// jest.mock("axios");
+const current_weather_endpoint =
+  "https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=bffa691e3dc38de5ea81dae512fdb94a&units=metric";
+
+const forecast_weather_endpoint =
+  "https://api.openweathermap.org/data/2.5/forecast/daily?q=Chicago&cnt=6&appid=bffa691e3dc38de5ea81dae512fdb94a&units=metric";
+
+const server = setupServer(
+  rest.get(current_weather_endpoint, (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(RAW_CURRENT_WEATHER_RESPONSE_SUCCESS));
+  }),
+  rest.get(forecast_weather_endpoint, (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(RAW_FORECAST_WEATHER_SUCCESS));
+  })
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 describe("<App/>", () => {
-  test("app should render without breaking", async () => {
-    // axios.get.mockImplementation(() =>
-    //   Promise.resolve(RAW_CURRENT_WEATHER_RESPONSE_SUCCESS)
-    // );
-    // axios.get.mockImplementationOnce(() =>
-    //   Promise.resolve(RAW_FORECAST_WEATHER_SUCCESS)
-    // );
-
-    //   axios.get.mockImplementationOnce(() =>
-    //   Promise.resolve(RAW_FORECAST_WEATHER_SUCCESS)
-    // );
-    // axios.get.mockImplementation(() =>
-    //   Promise.resolve(RAW_CURRENT_WEATHER_RESPONSE_SUCCESS)
-    // );
+  test("app should fetch weather data and render it to the screen", async () => {
     render(<App />);
-    //await wait();
+    await waitFor(() => screen.getByText("40"));
   });
 });
